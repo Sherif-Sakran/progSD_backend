0c517e5f301aa5415f9ef2451db2a186294b457d
Sherif-Sakran <sherifsakran@aucegypt.edu>
2024-10-25 22:57:02 +0100

    implemented, tested, and documented the rent function

--- progSD_Backend/users/views.py
+++ progSD_Backend/users/views.py
@@ -3,13 +3,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connection
 from django.http import JsonResponse
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.dateparse import parse_date
 import json
 from . import models
+from django.contrib.auth.forms import UserCreationForm 
+
 
 
 @csrf_exempt
@@ -92,7 +94,13 @@ def login_view(request):
         return JsonResponse({'error': 'Invalid request method'}, status=405)
     # return HttpResponse('Login is here')
 
-    
+@csrf_exempt
+def logout_view(request):
+    if request.method == "POST": 
+        logout(request) 
+        return JsonResponse({'message': 'You are now logged out'}, status=200)
+
+
 def fetch_data_vehicles(request):
     with connection.cursor() as cursor:
         cursor.execute('SELECT * FROM "Vehicles"')
@@ -222,4 +230,57 @@ def list_vehicles(request):
                 "is_defective": vehicle.is_defective,
             }
         )
-    return JsonResponse({'Vehicles': current_vehicles_json})
+    return JsonResponse({'All vehicles': current_vehicles_json})
+
+
+@csrf_exempt
+def list_available_vehicles_at(request):
+    if request.method == 'POST':
+        try:
+            location_json = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'message': 'Invalid JSON'}, status=400)
+        print('list available vehicles at a certain location')
+        current_vehicles = models.Vehicle.objects.filter(status='Available', location_id=location_json["location_id"])
+        current_vehicles_json = []
+        for vehicle in current_vehicles:
+            current_vehicles_json.append(
+                {
+                    "id": vehicle.id,
+                    "type": vehicle.type,
+                    "battery_level": vehicle.battery_level,
+                    "status": vehicle.status,
+                    "location_id": vehicle.location.id if vehicle.location else None,
+                    "last_maintenance_date": vehicle.last_maintenance_date.isoformat(),
+                    "is_defective": vehicle.is_defective,
+                }
+            )
+        return JsonResponse({'Vehicles': current_vehicles_json})
+
+
+    # choose location (list provided)
+    # choose a car (list of available car provided)
+@csrf_exempt
+def rent_vehicle(request):
+    # takes of the car and make its status as rented
+    if request.method == 'POST':
+        try:
+            vehicle_id_JSON = json.loads(request.body)
+        except json.JSONDecodeError:
+            return JsonResponse({'message': 'Invalid JSON'}, status=400)
+        print('received car id', vehicle_id_JSON["id"])
+        selected_vehicle = models.Vehicle.objects.filter(id=vehicle_id_JSON["id"])[0]
+        
+        
+        selected_vehicle_json = {
+            "id": selected_vehicle.id,
+            "type": selected_vehicle.type,
+            "battery_level": selected_vehicle.battery_level,
+            "status": selected_vehicle.status,
+            "location_id": selected_vehicle.location.id if selected_vehicle.location else None,
+            "last_maintenance_date": selected_vehicle.last_maintenance_date.isoformat(),
+            "is_defective": selected_vehicle.is_defective,
+        }
+        selected_vehicle.status = 'Rented'
+        selected_vehicle.save()
+        return JsonResponse({'Vehicle rented successfully': selected_vehicle_json})
