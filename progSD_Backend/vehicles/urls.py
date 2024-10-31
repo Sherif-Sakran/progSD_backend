dc382b72ff4ff19b5907ffc6e4d3eccbc92ef1ba
Sherif-Sakran <sherifsakran@aucegypt.edu>
2024-10-31 20:53:12 +0000

    separated the users app from the rest of the project

--- progSD_Backend/vehicles/urls.py
+++ progSD_Backend/vehicles/urls.py
@@ -4,7 +4,14 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
-    path('', views.available_vehicle, name=""),
-    # path('login/', views.login_view, name="login"),
+    # path('add_vehicle/', views.add_vehicle, name="add_vehicle"),
+    path('add_location/', views.add_location, name="add_location"),
+    path('list_locations/', views.list_locations, name="list_locations"),
+    path('add_vehicle/', views.add_vehicle, name="add_vehicle"),
+    path('rent_vehicle/', views.rent_vehicle, name="rent_vehicle"),
+    path('list_vehicles/', views.list_vehicles, name="list_vehicles"),
+    path('list_available_vehicles_at/', views.list_available_vehicles_at, name="list_available_vehicles_at"),
     # path('logout/', views.logout_view, name="logout"),
+    path('fetch-data/vehicles', views.fetch_data_vehicles, name='fetch_data_vehicles'),
+    path('list-tables/', views.list_tables, name='list_tables'),
 ]
