b65b544fddbfe6acc6ab31d5c0cf8a19aa74a505
Sherif-Sakran <sherifsakran@aucegypt.edu>
2024-10-31 22:11:21 +0000

    fixed a typo

--- progSD_Backend/users/models.py
+++ progSD_Backend/users/models.py
@@ -27,7 +27,7 @@ class CustomUser(AbstractUser):
     
     
     def __str__(self):
-        return self.usernam
+        return self.username
 
 class CustomerProfile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
